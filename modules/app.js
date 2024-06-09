5cfe10926188427aec769201e53a78a246ef1866
Nils Durieux <nils.durieux@etu.u-bordeaux.fr>
2024-06-09 19:56:39 +0200

    Added favicon + title animation

---
+++ modules/app.js
@@ -0,0 +1,18 @@
+/**
+     * Waits a certain amount of milliseconds, in the form of a promise.
+     * @param {*} ms Time in milliseconds to wait for.
+     * @returns The timeout promise.
+     */
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+
+const main = async () => {
+    const docTitle = document.title;
+    while(true){
+        document.title = docTitle+' _';
+        await sleep(500);
+        document.title = docTitle;
+        await sleep(500);
+    }
+}
+
+main();
